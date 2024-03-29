b9179665899e403fc3094915c482e3a80338c515
darasdarmawan <darasyauqidarmawan@gmail.com>
2024-03-29 09:57:37 +0700

    while i <= j

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -51,4 +51,23 @@ void q_short(int low, int high)
 	i = low + 1;
 	j = high;
 
+	while (i <= j)
+	{
+		while ((arr[i] <= pivot) && (i <= high))
+		{
+			i++;
+			cmp_count++;
+		}
+		cmp_count++;
+		while ((arr[j] > pivot) && (j >= low))
+		{
+			j--;
+			cmp_count++;
+		}
+		cmp_count++;
+		if (i < j)
+		{
+			swap(i, j);
+		}
+	}
 }
