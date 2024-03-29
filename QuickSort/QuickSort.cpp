e8cb780d7b7f283f6d95dd549a69929a1d694266
darasdarmawan <darasyauqidarmawan@gmail.com>
2024-03-29 10:05:21 +0700

    menambahkan for

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -86,4 +86,11 @@ void display()
 	cout << "Sorted Array" << endl;
 	cout << "-------------------" << endl;
 
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << " ";
+	}
 
+	cout << "\n\nNumber of comparasion: " << cmp_count << endl;
+	cout << "Number of data movements: " << mov_count << endl;
+}
